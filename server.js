206477b7494c25df5397cb801be045a5f8c28c2e
jordan <jordan@jordans-MacBook-Air.local>
2020-01-24 16:51:24 +0700

    got projects rendering

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express= require ("express");
-
+const projectRouter= require("./projects/project-router")
 const server = express();
+
+
+server.use(express.json());
+
+server.use("/api/projects", projectRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1>Sprint Challenge</h1>");
